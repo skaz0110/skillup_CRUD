d956f13944770dc42f03f58f8b3e6e06bfbb581c
かずき <skaz0110@yahoo.co.jp>
2020-11-30 23:57:55 +0900

    resourcesの練習

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+    def index
+        
+    end
+    
+    def new
+        @users = User.new
+    end
+
+    def create
+        
+    end
+    
+    
+end
