84d963273ffb868de64e6a63749cb52b27e617fa
かずき <skaz0110@yahoo.co.jp>
2020-12-01 20:56:09 +0900

    テストコミット

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,20 @@
 class UsersController < ApplicationController
     def index
-        
+        @users = User.all
     end
     
     def new
-        @users = User.new
+        @user = User.new
     end
 
     def create
-        
+        User.create(user_params)
+        redirect_to users_path
     end
     
+    private
+    def user_params
+        params.require(:user).permit(:name,:age)
+    end
     
 end
